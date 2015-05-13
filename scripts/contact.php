d6fed2af362ef16f50135b5c6fcf35de4b349dad
nsi223 <nsi223@yandex.ru>
2015-05-13 18:34:06 +0300

    Поменял почту

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
      if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage']) && !empty($_POST['contactnewfield'])) {
-	$to = 'nsi223@yandex.ru'; // Your e-mail address here.
+	$to = 'support@instagrammy.ru'; // Your e-mail address here.
  $body = "\nName: {$_POST['contactname']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactmessage']}\n\n{$_POST['contactnewfield']}\n\n";
-	mail($to, "NEW ZAKAZ MAFAKA", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+	mail($to, "INSTAGRAMMY NEW CLIENT", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
     }
 }
 ?>
